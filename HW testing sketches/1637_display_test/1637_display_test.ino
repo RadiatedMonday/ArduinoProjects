d5c5c21b6013c20ad2372bcf4aeff1d00404b9f9
Cognoscibilis <cognoscibilis@gmail.com>
2025-03-08 19:08:09 +0300

    testing sketches

---
+++ HW testing sketches/1637_display_test/1637_display_test.ino
@@ -0,0 +1,17 @@
+#define CLK 12
+#define DIO 11
+
+#include "GyverTM1637.h"
+GyverTM1637 disp(CLK, DIO);
+
+uint32_t Now, clocktimer;
+boolean flag;
+
+void setup() {
+  Serial.begin(9600);
+  disp.clear();
+  disp.brightness(7);  // яркость, 0 - 7 (минимум - максимум)
+  disp.clear();
+  disp.displayByte(_H, _E, _L, _L);
+}
+void loop(){}
